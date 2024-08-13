d97b01ac916c27a0a8699e674d3885f3f2a27120
VINÍCIUS DIEFENBACH <0406981@feevale.br>
2024-08-13 19:58:13 -0300

    Solucao do primeiro exercício da revisão de algoritmos

--- revisao-algoritmos/src/Um.java
+++ revisao-algoritmos/src/Um.java
@@ -1,4 +1,11 @@
 
 public class Um {
+	private final static int NUMERO_DE_CARACTERES = 256;
 
+	public static void main(String[] args) {
+		for (int i = 0; i < NUMERO_DE_CARACTERES; i += 1) {
+//			if (i >= 'A' && i <= 'Z' || i >= 'a' && i <= 'z')
+			System.out.printf("%d => %c\n", i, i);
+		}
+	}
 }
